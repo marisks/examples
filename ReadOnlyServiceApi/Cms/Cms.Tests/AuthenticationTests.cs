0eeff669c4a0b4f95c10196809ebfd36b81d57dd
Māris Krivtežs <marisks@gmail.com>
2016-11-22 18:41:27 +0200

    Added authentication test.

---
+++ ReadOnlyServiceApi/Cms/Cms.Tests/AuthenticationTests.cs
@@ -0,0 +1,14 @@
+﻿using Cms.Tests.Base;
+using Xunit;
+
+namespace Cms.Tests
+{
+    public class AuthenticationTests : ApiTestsBase
+    {
+        [Fact]
+        public void it_authenticates()
+        {
+            Assert.True(true);
+        }
+    }
+}
