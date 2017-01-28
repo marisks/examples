e947199a8b7bbbfe049102e28a7e043d2ea7b0ea
Māris Krivtežs <marisks@gmail.com>
2017-01-28 09:32:08 +0200

    Added test gadget.

---
+++ DIStatus/Cms/Cms/Controllers/TestGadgetController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using EPiServer.Shell.Gadgets;
+
+namespace Cms.Controllers
+{
+    [Gadget]
+    public class TestGadgetController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("<h1>Test</h1>");
+        }
+    }
+}
